14e4f6bc9af4680a9feb5b08a170a6d099678d60
Mark Hoad <markdhoad@gmail.com>
2017-02-18 15:22:01 +0000

    Posts now calculate their own reading time and display it on the post.

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,24 @@ class Post < ApplicationRecord
   validates :title, presence: true
   validates :body, presence: true
   belongs_to :category
+
+  before_save :calculate_reading_time
+
+  private
+
+  def calculate_reading_time
+    image_comprehension_time = 0.5 * calculate_image_count
+    reading_comprehension_time = calculate_word_count / 275
+    self.reading_time = (image_comprehension_time + reading_comprehension_time).ceil
+  end
+
+  def calculate_word_count
+    html_free_post = ActionView::Base.full_sanitizer.sanitize(body).squish
+    html_free_post.scan(/[\w-]+/).size
+  end
+
+  def calculate_image_count
+    # Find how many images are there in the post content
+    body.scan(/<img([\w\W]+?)>/).size
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170218145943) do
+ActiveRecord::Schema.define(version: 20170218151524) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20170218145943) do
     t.integer  "category_id"
     t.string   "facebook_description"
     t.string   "meta_description"
+    t.integer  "reading_time"
     t.index ["category_id"], name: "index_posts_on_category_id", using: :btree
   end
 
